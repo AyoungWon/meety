76b474ef0d895abeadab49a5076b38e3c2697f70
AyoungWon <thwn4750@gmail.com>
2021-02-05 14:10:07 +0900

    node login

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const { User } = require('./models/Users') 
 const config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -31,6 +34,39 @@ app.post('/register', (req,res)=> {
 
 })
 
+app.post('/login', (req,res) => {
+  //요청된 이메일을 데이터베이스에서 찾기
+  User.findOne({ email: req.body.email },(err, user)=> {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message:"이메일에 해당하는 로그안 정보가 없습니다"
+      })
+    }
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err,isMatch) => {
+      if(!isMatch)
+      return res.json({
+        loginSuccess: false,
+        message:"비밀번호 오류"
+      })
+      user.generateToken((err,user) => {
+        if(err) return res.status(400).send(err);
+        //토큰을 (쿠키, 로컬스토리지 등에) 저장한다.
+        res.cookie("x_auth",user.token) 
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+      })
+
+    } )
+  })
+  
+
+  //비밀번호가 맞다면 토큰생성
+})
 
 
 
